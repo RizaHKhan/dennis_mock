9ab2da9afaf53aeec1ad85036d3536dff2476222
khanr <khanriza@gmail.com>
2021-04-22 20:27:32 -0400

    added logged in state to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    loggedIn: false,
+  },
+  mutations: {
+    logIn(state, condition) {
+      state.loggedIn = condition;
+    },
+  },
   actions: {},
+  getters: {
+    isLoggedIn: (state) => state.loggedIn === true,
+  },
   modules: {},
 });
