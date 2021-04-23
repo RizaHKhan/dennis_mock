2d52050f49dfb8704f6f75986c7449e5fe3ae0b3
khanr <khanriza@gmail.com>
2021-04-22 20:28:06 -0400

    added router guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,43 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import store from "../store";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      auth: true,
+    },
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      auth: false,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth) {
+    if (store.getters.isLoggedIn) next();
+    next({ name: "Login" });
+  }
+
+  if (!to.meta.auth) {
+    if (store.getters.isLoggedIn) next({ name: "Home" });
+    if (to.name !== "Login") next({ name: "Login" });
+    next();
+  }
 });
 
 export default router;
